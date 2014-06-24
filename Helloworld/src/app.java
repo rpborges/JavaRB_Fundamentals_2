37945b431a9940b93019b1862e3cc480ee662faa
hasff <hugoferro@gmail.com>
2014-06-24 21:41:27 +0100

    Primeiro Commit

---
+++ Helloworld/src/app.java
@@ -0,0 +1,14 @@
+/**
+ * A minha primeira aplicação Java
+ * 
+ * @author Rui Borges
+ * 
+ */
+public class app {
+	
+	public static void main(String[] args) {
+		// vou imprimir
+		System.out.println("Hello");
+	}
+
+}
