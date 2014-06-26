cfc1b34b006b6f581e436a3dbdf36031c3a33b67
rpborges <rui.borges@kcsit.pt>
2014-06-26 21:58:20 +0100

    $1

--- Helloworld/src/app.java
+++ Helloworld/src/app.java
@@ -5,10 +5,22 @@
  * 
  */
 public class app {
-	
+
 	public static void main(String[] args) {
 		// vou imprimir
-		System.out.println("Hello !!!----");
+		System.out.println("HELLO");
+		for (int i = 0; i < args.length; i++)
+			if (i < args.length - 1)
+				System.out.print(args[i] + " - ");
+			else
+				System.out.print(args[i]);
+		
+		System.out.println();
+
+		for (int i = 0; i < args.length; i++)
+			System.out.print(args[i] + ((i == args.length - 1) ? "" : " "));
+
+		System.out.println();
 	}
 
 }
